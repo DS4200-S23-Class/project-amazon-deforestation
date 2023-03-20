b7db6e633ec252968466eaf71f6d64037c9f3669
Luke Abbatessa <abbatessa.l@northeastern.edu>
2023-03-20 00:32:11 -0400

    Made edits to Visual Encoding 1

--- js/main.js
+++ js/main.js
@@ -10,52 +10,55 @@ const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
-// Create a frame for Visual Encoding 1
-const FRAME1 = d3.select("#vis-enc-1")
-                  .append("svg")
-                    .attr("height", FRAME_HEIGHT)
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame");
-
 // Read in the data
 d3.csv("avg_def_data.csv").then((data) => {
 
-	// Initialize the maximum y value
-	const MAXy = d3.max(data, (d) => { return parseInt(d.Average_Proportion_Area_Deforested); });
+	// Create the scales and bin range for the histogram
+	const ySCALE_REV = d3.scaleLinear() 
+	    .domain([0, d3.max(data, (d) => { return d.Average_Proportion_Area_Deforested; })])  
+	    .range([VIS_HEIGHT, 0]);
 
-	// Create the y scale
-	const ySCALE_REV = d3.scaleLinear()
-	                   .domain([0, MAXy])
-	                   .range([VIS_HEIGHT, 0]);
-
-	// Create the x scale
 	const xSCALE = d3.scaleLinear()
-	               .range([ 0, VIS_WIDTH ])
-	               .domain([2000, 2021]);
+	    .domain([d3.min(data, function(d) { return d.Range_Minimums; }), d3.max(data, function(d) { return d.Range_Maximums; })])
+	    .range([0, VIS_WIDTH]);
 
-	// Create the x-axis
+	const histogram = d3.histogram()
+	    .value(function(d) { return d.Midyear; })
+	    .domain(xSCALE.domain())
+	    .thresholds(xSCALE.ticks(7));
+
+	const BINS = histogram(data);
+
+	// Append the bars to the FRAME1 element
+	const FRAME1 = d3.select("#vis-enc-1").append("svg")
+        .attr("height", FRAME_HEIGHT)
+        .attr("width", FRAME_WIDTH)
+        .attr("class", "frame");
+
+    FRAME1.selectAll("rect")
+        .data(BINS)
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { return xSCALE(d.Range_Minimums); })
+        .attr("y", function(d) { return VIS_HEIGHT - ySCALE_REV(d.Average_Proportion_Area_Deforested); })
+        .attr("width", function(d) { return xSCALE(d.Range_Maximums) - xSCALE(d.Range_Minimums) + 1; })
+        .attr("height", function(d) { return ySCALE_REV(d.Average_Proportion_Area_Deforested); })
+        .style("fill", "rosybrown")
+        .style("stroke", "saddlebrown");
+
+    // Create the x-axis
 	FRAME1.append("g")
-	   .attr("transform", "translate(" + MARGINS.left +
-	   	"," + (VIS_HEIGHT + MARGINS.bottom) + ")")
-	   .call(d3.axisBottom(xSCALE))
-	   .selectAll("text")
-	     .attr("font-size", "10px");
+	    .attr("transform", "translate(" + MARGINS.left + 
+		   "," + (VIS_HEIGHT + MARGINS.bottom) + ")")
+	    .call(d3.axisBottom(xSCALE))
+	    .selectAll("text")
+		  .attr("font-size", "10px");
 
 	// Create the y-axis
 	FRAME1.append("g")
-	   .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")")
-	   .call(d3.axisLeft(ySCALE_REV))
-	   .selectALL("text")
-	     .attr("font-size", "10px");
-
-	// Create the bars
-	FRAME1.selectAll("bar")
-	.data(data)
-	.enter()
-	.append("rect")
-	  .attr("x", function(d) { return xSCALE(d.Midyear) + MARGINS.left; })
-	  .attr("y", function(d) { return ySCALE_REV(d.Average_Proportion_Area_Deforested) + MARGINS.top; })
-	  .attr("height", function(d) { return VIS_HEIGHT - ySCALE(d.Average_Proportion_Area_Deforested); })
-	  .attr("class", "bar");
+	    .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")") 
+	    .call(d3.axisLeft(ySCALE_REV))
+	    .selectAll("text")
+		  .attr("font-size", "10px");
 
 });
